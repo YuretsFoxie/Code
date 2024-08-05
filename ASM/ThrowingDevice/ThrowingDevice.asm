7b6cd68e448756abc4a2e16aa7abc4453aff3116
Foxie <9999dogs2@gmail.com>
2024-08-05 15:37:05 +0300

    Throwing device implementation in progress: fix needed (now the motor turns both sides instead of being idle).

--- ASM/ThrowingDevice/ThrowingDevice.asm
+++ ASM/ThrowingDevice/ThrowingDevice.asm
@@ -1,20 +1,28 @@
 .device  ATtiny13A
 .include "tn13Adef.inc"
 
-.equ F_SYSTEM	= 4800000
-.equ PRESCALER	= 1024
-.equ F_MAIN		= 50
+.equ F_SYSTEM			= 4800000
+.equ PRESCALER			= 1024
+.equ F_MAIN				= 50
 
 ; TODO: adjust the values
+.equ F_MIN				= 400	; 500
+.equ F_MID				= 600	; 750
+.equ F_MAX				= 1600	; 1000
 
-.equ F_MIN		= 400	; 500
-.equ F_MID		= 600	; 750
-.equ F_MAX		= 1600	; 1000
+.equ MATCH_MAIN			= F_SYSTEM / (F_MAIN * PRESCALER) - 1
+.equ MATCH_MIN			= F_SYSTEM / (F_MIN * PRESCALER) - 1
+.equ MATCH_MID			= F_SYSTEM / (F_MID * PRESCALER) - 1
+.equ MATCH_MAX			= F_SYSTEM / (F_MAX * PRESCALER) - 1
 
-.equ MATCH_MAIN	= F_SYSTEM / (F_MAIN * PRESCALER) - 1
-.equ MATCH_MIN	= F_SYSTEM / (F_MIN * PRESCALER) - 1
-.equ MATCH_MID	= F_SYSTEM / (F_MID * PRESCALER) - 1
-.equ MATCH_MAX	= F_SYSTEM / (F_MAX * PRESCALER) - 1
+; TODO: adjust the values
+.equ ADCThreshold		= 0b11101100
+.equ ADCMatchesCount	= 0b01000000
+.equ DelayNumber		= 0b00001000
+.equ DelayHalfNumber	= 0b00000100
+
+.def DelayCounter		= R17
+.def ADCCounter			= R18
 
 ;=====
 
@@ -27,6 +35,22 @@ out	 @0, R16
 Load SPL, low(RAMEND)
 .endm
 
+.macro SetIsTurnedRight
+set
+.endm
+
+.macro ClearIsTurnedRight
+clt
+.endm
+
+.macro CheckIsTurnedRight
+brts @0
+.endm
+
+.macro InitWatchdog
+Load WDTCR, 1<<WDTIE | 1<<WDP2 | 1<<WDP0	; Watchdog interrupt is called each 0.5 second, 1<<WDP2 | 1<<WDP1 for 1 second
+.endm
+
 .macro InitTimer0
 Load OCR0A,  MATCH_MAIN
 Load OCR0B,  MATCH_MID
@@ -36,9 +60,21 @@ Load TIMSK0, 1<<OCIE0A | 1<<OCIE0B			; Enable the Compare Match interrupts
 Load TIFR0,  0b11111111					; Clear the interrupt flags
 .endm
 
+.macro InitADC
+Load ADMUX,  0<<REFS0 | 1<<ADLAR | 0<<MUX1 | 1<<MUX0									; Vcc is used as voltage reference, result is left-adjusted, ADC1 (PB2 is input)
+Load ADCSRA, 1<<ADEN | 1<<ADSC | 1<<ADATE | 1<<ADIE | 1<<ADPS2 | 1<<ADPS1 | 1<<ADPS0	; ADC enabled, start conversion, trigger source enabled (for the free running mode), interrupt enabled, prescaler 128
+Load ADCSRB, 0<<ADTS2 | 0<<ADTS1 | 0<<ADTS0												; Free running mode enabled
+.endm
+
+.macro InitExternalInterrupt
+Load MCUCR, 0<<ISC01 | 0<<ISC00
+Load GIMSK, 1<<PCIE
+Load PCMSK, 1<<PCINT1
+.endm
+
 .macro InitPortB
-sbi DDRB, PB0
-sbi PORTB, PB0
+Load DDRB,  0b00010001
+Load PORTB, 0b00000110
 .endm
 
 .macro DisableComparator
@@ -53,7 +89,10 @@ sei
 
 .macro Init
 InitStack
+InitWatchdog
 InitTimer0
+InitADC
+InitExternalInterrupt
 InitPortB
 DisableComparator
 EnableInterrupts
@@ -67,14 +106,23 @@ rjmp Main
 ;=====
 
 .org 0x0000 rjmp Reset
+.org 0x0002 rjmp ExternalInterrupt		; 1 for INT0, 2 for PCINTs
 .org 0x0006 rjmp TimerCompareMatchA
 .org 0x0007 rjmp TimerCompareMatchB
+.org 0x0008 rjmp WatchdogTimeout
+.org 0x0009 rjmp ADCConversionComplete
 
 ;=====
 
 Reset:
 Init
 Run
+
+;=====
+
+ExternalInterrupt:
+ldi DelayCounter, DelayNumber
+reti
 
 ;=====
 
@@ -89,3 +137,93 @@ cbi PORTB, PB0
 reti
 
 ;=====
+
+WatchdogTimeout:
+sbi  PINB, PB4
+
+cpi  DelayCounter, 0
+breq ReturnWatchdogTimeout
+
+cpi  DelayCounter, DelayNumber
+breq TurnLeftOrRight
+
+cpi  DelayCounter, DelayHalfNumber
+breq TurnMiddle
+
+dec  DelayCounter
+ReturnWatchdogTimeout:
+cbi DDRB, PB0
+reti
+
+TurnLeftOrRight:
+sbi DDRB, PB0
+dec DelayCounter
+CheckIsTurnedRight IsTurnedRight
+SetIsTurnedRight
+Load OCR0B, MATCH_MIN
+reti
+
+IsTurnedRight:
+ClearIsTurnedRight
+Load OCR0B, MATCH_MAX
+reti
+
+TurnMiddle:
+Load OCR0B, MATCH_MID
+dec DelayCounter
+reti
+
+;=====
+
+ADCConversionComplete:
+in   R16, ADCH
+cpi  R16, ADCThreshold
+brlo IsVoltageLess
+
+cpi  ADCCounter, ADCMatchesCount
+breq ADCCounterIsFull
+inc  ADCCounter
+reti
+
+ADCCounterIsFull:
+ldi DelayCounter, DelayNumber
+reti
+
+IsVoltageLess:
+cpi  ADCCounter, 0
+breq ADCCounterIsEmpty
+dec  ADCCounter
+reti
+
+ADCCounterIsEmpty:
+reti
+
+;=====
+
+/*
+onADCConversionComplete()
+{
+	if (ADCH >= ADCThreshold)
+	{
+		if (ADCCounter < ADCMatchesCount)
+		{
+			ADCCounter++;
+		}
+		else if (ADCCounter == ADCMatchesCount)
+		{
+			ledOn();
+		}
+	}
+	else
+	{
+		if (ADCCounter > 0)
+		{
+			ADCCounter--;
+		}
+		else if (ADCCounter == 0)
+		{
+			ledOff();
+		}
+	}
+}
+*/
