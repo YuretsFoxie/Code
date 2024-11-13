a588eec72dd25ef35cfbc953a85fc7793e398f20
Foxie <9999dogs2@gmail.com>
2024-11-13 22:47:03 +0200

    Updated the TODO file.

--- C++/UARTtoOpenGL/TODO.cpp
+++ C++/UARTtoOpenGL/TODO.cpp
@@ -1,19 +1,12 @@
-// TODO: Implement the Subwindow functionality.
 // TODO: Implement the fullscreen mode.
+// TODO: Use the Subwindow functionality for rendering.
 // TODO: Implement the spectrum analyzer functionality.
-
-Please, implement handling the key press events: 
-1. If ESC is pressed, the application quits.
-2. If F1 is pressed, UART data receiving is toggled. Send "1" to start data transmission, send "0" to stop data transmission. 
-Preserve the original code formatting, do not replace the tabs with the spaces and keep 3 lines between the classes. 
-Please, provide two halves of the updated code in two different messages.
-
-Please, provide the second half of the updated code, which matches the first one.
 
 https://github.com/YuretsFoxie/Code
 
-
-
+Preserve the original code formatting, do not replace the tabs with the spaces and keep 3 lines between the classes. 
 
 Please, do not use the placeholder comments, implement the actual functionality. 
 Please, provide two halves of the updated code in two different messages. The havles' size should be close enough, so they both can fit to the messages.
+
+Please, provide the second half of the updated code, which matches the first one.
